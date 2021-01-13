f39affdfead48ec27677ecf982f67cc84bd9ef11
SchrodingerZhu <i@zhuyi.fan>
2021-01-13 10:20:16 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 class Grammar {
 public:
     [[nodiscard]]  virtual std::optional<size_t> topdown_match(std::string_view context, size_t index) const = 0;
-    [[nodiscard]]  virtual Grammar* instance() const = 0;
+
+    [[nodiscard]]  virtual Grammar *instance() const = 0;
 };
 
 #define GRAMMAR_INSTANCE(Type) Grammar* instance() const override { \
@@ -18,6 +19,7 @@ public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
         return index == 0 ? std::make_optional(0) : std::nullopt;
     }
+
     GRAMMAR_INSTANCE(Start);
 };
 
@@ -26,30 +28,33 @@ public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
         return index >= context.size() ? std::make_optional(0) : std::nullopt;
     }
+
     GRAMMAR_INSTANCE(End);
 };
 
 // Lexical Definitions
-template <char C>
+template<char C>
 class Char : public Grammar {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
         return context[index] == C ? std::make_optional(1) : std::nullopt;
     }
+
     GRAMMAR_INSTANCE(Char);
 };
 
-template <char S, char T>
+template<char S, char T>
 class CharRange : public Grammar {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
         return context[index] >= S && context[index] <= T ? std::make_optional(1) : std::nullopt;
     }
+
     GRAMMAR_INSTANCE(CharRange);
 };
 
 // utilities
-template <class Clause>
+template<class Clause>
 class Plus : public Grammar {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
@@ -61,12 +66,13 @@ public:
             count += *current;
             current = Clause().topdown_match(context, index + count);
         }
-        return flag ?  std::make_optional(count) : std::nullopt;
+        return flag ? std::make_optional(count) : std::nullopt;
     }
+
     GRAMMAR_INSTANCE(Plus);
 };
 
-template <class Clause>
+template<class Clause>
 class Asterisk : public Grammar {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
@@ -78,33 +84,37 @@ public:
         }
         return std::make_optional(count);
     }
+
     GRAMMAR_INSTANCE(Asterisk);
 };
 
-template <class Clause0, class ... Clauses>
+template<class Clause0, class ... Clauses>
 class Seq : public Seq<Clauses...> {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
         auto fst = Clause0().topdown_match(context, index);
         if (fst) {
-            return *fst + Seq<Clauses...>::topdown_match(context, index + *fst).template value_or(0);
+            auto snd = Seq<Clauses...>::topdown_match(context, index + *fst);
+            return snd ? std::make_optional(*fst + *snd) : std::nullopt;
         } else {
             return std::nullopt;
         }
     }
+
     GRAMMAR_INSTANCE(Seq);
 };
 
-template <class Clause>
+template<class Clause>
 class Seq<Clause> : public Grammar {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
         return Clause().topdown_match(context, index);
     }
+
     GRAMMAR_INSTANCE(Seq);
 };
 
-template <class Clause0, class ... Clauses>
+template<class Clause0, class ... Clauses>
 class Ord : public Ord<Clauses...> {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
@@ -115,15 +125,17 @@ public:
             return Ord<Clauses...>::topdown_match(context, index);
         }
     }
+
     GRAMMAR_INSTANCE(Ord);
 };
 
-template <class Clause>
+template<class Clause>
 class Ord<Clause> : public Grammar {
 public:
     [[nodiscard]] std::optional<size_t> topdown_match(std::string_view context, size_t index) const override {
         return Clause().topdown_match(context, index);
     }
+
     GRAMMAR_INSTANCE(Ord);
 };
 
@@ -137,8 +149,11 @@ class Name : public __VA_ARGS__ { \
 // Grammar
 
 class Additive;
+
 class Multicative;
+
 class Primary;
+
 class Toplevel;
 
 CLAUSE(Digit, CharRange<'0', '9'>)
@@ -153,7 +168,22 @@ CLAUSE(Primary, Ord<Seq<Char<'('>, Additive, Char<')'>>, Number>)
 
 CLAUSE(Toplevel, Seq<Start, Additive, End>)
 
+struct MemoKey {
+    Grammar *instance;
+    size_t index;
+};
+
+constexpr static uint64_t KEY[2]{0x243f6a8885a308d3ull,
+                                 0x13198a2e03707344ull};
+
+struct MemoKeyHasher {
+    size_t operator()(const MemoKey & key) {
+        return std::hash<void *>{}(key.instance) ^ std::hash<size_t>{}(-key.index);
+    }
+};
+
 int main() {
-    std::cout << Toplevel().topdown_match("(1+1)+1", 0).value_or(0) << std::endl;
+    std::cout << Toplevel().topdown_match("(1+1)+1*(5+5)", 0).value_or(0) << std::endl;
+    std::cout << MemoKeyHasher()(MemoKey{.instance = static_cast<Grammar *>(malloc(1)), .index = 11});
     return 0;
 }
